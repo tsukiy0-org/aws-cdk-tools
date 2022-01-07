07759afcd78989f92fc7a1a55699002856fc4e36
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-01-07 23:00:21 +1100

    feat: add noIndex prop to prevent search engine indexing
    * useful for staging websites

--- packages/aws-cdk-tools/src/constructs/S3StaticSite.ts
+++ packages/aws-cdk-tools/src/constructs/S3StaticSite.ts
@@ -1,4 +1,4 @@
-import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { ICertificate } from "aws-cdk-lib/aws-certificatemanager";
 import {
   BehaviorOptions,
   CachePolicy,
@@ -6,11 +6,12 @@ import {
   ICachePolicy,
   IDistribution,
   OriginProtocolPolicy,
-} from 'aws-cdk-lib/aws-cloudfront';
-import { HttpOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { BucketDeployment, ISource } from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
-import { DefaultBucket } from './DefaultBucket';
+  ResponseHeadersPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { BucketDeployment, ISource } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+import { DefaultBucket } from "./DefaultBucket";
 
 export class S3StaticSite extends Construct {
   private readonly cdn: IDistribution;
@@ -25,17 +26,18 @@ export class S3StaticSite extends Construct {
         certificate: ICertificate;
         domainName: string;
       };
+      noIndex?: boolean;
     }
   ) {
     super(scope, id);
 
-    const bucket = new DefaultBucket(this, 'Bucket', {
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: '404.html',
+    const bucket = new DefaultBucket(this, "Bucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "404.html",
       publicReadAccess: true,
     });
 
-    new BucketDeployment(this, 'BucketDeployment', {
+    new BucketDeployment(this, "BucketDeployment", {
       destinationBucket: bucket,
       sources: [props.source],
     });
@@ -44,12 +46,29 @@ export class S3StaticSite extends Construct {
       protocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
     });
 
-    const cdn = new Distribution(this, 'CloudFront', {
+    const headersPolicy = new ResponseHeadersPolicy(this, "HeadersPolicy", {
+      customHeadersBehavior: {
+        customHeaders: [
+          ...(props.noIndex
+            ? [
+                {
+                  header: "x-robots-tag",
+                  value: "noindex, nofollow",
+                  override: true
+                },
+              ]
+            : []),
+        ],
+      },
+    });
+
+    const cdn = new Distribution(this, "CloudFront", {
       certificate: props.domainName?.certificate,
       domainNames: props.domainName ? [props.domainName.domainName] : undefined,
       defaultBehavior: {
         origin,
         cachePolicy: CachePolicy.CACHING_DISABLED,
+        responseHeadersPolicy: headersPolicy,
       },
       additionalBehaviors: props.cacheBehaviors.reduce<
         Record<string, BehaviorOptions>
@@ -59,6 +78,7 @@ export class S3StaticSite extends Construct {
           [next.path]: {
             origin,
             cachePolicy: next.cachePolicy,
+            responseHeadersPolicy: headersPolicy,
           },
         };
       }, {}),
